bcf7664ef95ac9494ce16888b6625a2ef3280e95
TommyArnzen123 <ThomasArnzen@gmail.com>
2025-09-16 22:16:11 -0400

    Delete Device Confirmation Modal Added
    Confirmation modal added for the delete device button.
    
    Delete Device button now displayed based on if the device ID exists in the database.

--- src/app/model/modal.interface.ts
+++ src/app/model/modal.interface.ts
@@ -2,8 +2,10 @@ export interface IModal {
   title: string;
   content: string;
   primaryText?: string;
+  secondaryText?: string;
 }
 
 export interface IModalActions {
   primaryAction?: () => void;
+  secondaryAction?: () => void;
 }
