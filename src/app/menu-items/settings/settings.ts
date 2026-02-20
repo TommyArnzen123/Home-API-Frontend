49e826857459dcdab220127752ad58cb68cdb20f
TommyArnzen123 <ThomasArnzen@gmail.com>
2026-02-20 01:08:29 -0500

    Settings Page Toggle Buttons Added
    Update to add toggle buttons to the settings page for the temperature display, time display, and theme use-cases.

--- src/app/menu-items/settings/settings.ts
+++ src/app/menu-items/settings/settings.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+type temperatureTypes = 'fahrenheit' | 'celsius';
+type timeTypes = '12' | '24';
+type colorModeTypes = 'light' | 'dark';
 
 @Component({
   selector: 'settings',
-  imports: [],
+  imports: [MatButtonToggleModule, FormsModule],
   templateUrl: './settings.html',
   styleUrl: './settings.scss',
 })
-export class Settings {}
+export class Settings {
+  protected temperatureSetting: temperatureTypes = 'fahrenheit';
+  protected timeSetting: timeTypes = '24';
+  protected colorModeSetting: colorModeTypes = 'light';
+}
