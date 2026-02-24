d2a19c6d6da7e552dacb38e279745847d8fb2e7e
TommyArnzen123 <ThomasArnzen@gmail.com>
2026-02-23 20:00:01 -0500

    Remove Captive Error Subscription Variable
    Remove subscription variable in the captive error component as it is not being used.

--- src/app/captive-error/captive-error.ts
+++ src/app/captive-error/captive-error.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit, OnDestroy } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
 import { HOME_SCREEN_INFO_ERROR } from './captive-error-constants';
 
 @Component({
@@ -9,9 +8,7 @@ import { HOME_SCREEN_INFO_ERROR } from './captive-error-constants';
   templateUrl: './captive-error.html',
   styleUrl: './captive-error.scss',
 })
-export class CaptiveError implements OnInit, OnDestroy {
-  subscriptions: Subscription[] = [];
-
+export class CaptiveError implements OnInit {
   homeScreenInfoError: string | null = null;
 
   private readonly activatedRoute = inject(ActivatedRoute);
@@ -19,9 +16,5 @@ export class CaptiveError implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.homeScreenInfoError =
       this.activatedRoute.snapshot.queryParamMap.get(HOME_SCREEN_INFO_ERROR);
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 }
